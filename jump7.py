1e93969dd2877d2d157611e40ba89c6a2b8e718c
snnu121 <66044107+snnu121@users.noreply.github.com>
2022-12-21 11:18:00 +0800

    Add files via upload

---
+++ jump7.py
@@ -0,0 +1,12 @@
+for a in range(1,101):
+    if a % 7 == 0:
+        continue
+    else:
+        if a % 10 == 7:
+           continue
+        else:
+        	if a // 10 == 7:
+        		continue
+        	else:
+        		print(a)
+         
